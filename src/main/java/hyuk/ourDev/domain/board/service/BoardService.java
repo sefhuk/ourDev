a6a99e5e3a918ffe10fd7cfafac48492edf4eeba
sefhuk <ish0411@naver.com>
2024-04-22 18:52:29 +0900

    feat: 게시판 목록 요청 Service 구현

---
+++ src/main/java/hyuk/ourDev/domain/board/service/BoardService.java
@@ -0,0 +1,18 @@
+package hyuk.ourDev.domain.board.service;
+
+import hyuk.ourDev.domain.board.entity.Board;
+import hyuk.ourDev.domain.board.repository.JdbcTemplateRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final JdbcTemplateRepository jdbcTemplateRepository;
+
+    public List<Board> findBoards() {
+        return jdbcTemplateRepository.findAll();
+    }
+}
