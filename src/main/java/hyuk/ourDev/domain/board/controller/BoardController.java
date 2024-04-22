5dd15981d7ae7151a336faa13bad3abe9f01b91d
sefhuk <ish0411@naver.com>
2024-04-22 18:41:33 +0900

    feat: index 페이지 요청 Controller 구현

--- src/main/java/hyuk/ourDev/domain/board/controller/BoardController.java
+++ src/main/java/hyuk/ourDev/domain/board/controller/BoardController.java
@@ -1,15 +1,13 @@
 package hyuk.ourDev.domain.board.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping
+@Controller
 public class BoardController {
 
     @GetMapping
     public String index() {
-        return "Hello, World!";
+        return "index";
     }
 }
