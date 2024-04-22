6efacfbcfa7dc8eb49a7703eb91e3d3d68a3640e
sefhuk <ish0411@naver.com>
2024-04-22 18:57:30 +0900

    feat: 게시판 목록 요청 Controller 구현

--- src/main/java/hyuk/ourDev/domain/board/controller/BoardController.java
+++ src/main/java/hyuk/ourDev/domain/board/controller/BoardController.java
@@ -1,13 +1,29 @@
 package hyuk.ourDev.domain.board.controller;
 
+import hyuk.ourDev.domain.board.entity.Board;
+import hyuk.ourDev.domain.board.service.BoardService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
+
+    public final BoardService boardService;
 
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/board")
+    public String boardList(Model model) {
+        List<Board> boards = boardService.findBoards();
+        model.addAttribute("boards", boards);
+
+        return "boards";
     }
 }
