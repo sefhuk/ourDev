0d95cd71f571bd1e4a24ae427d7dbcd3787b59bf
sefhuk <ish0411@naver.com>
2024-04-25 15:07:30 +0900

    feat: BoardService 트랜잭션 추가

--- src/main/java/hyuk/ourDev/domain/board/service/BoardService.java
+++ src/main/java/hyuk/ourDev/domain/board/service/BoardService.java
@@ -6,9 +6,11 @@ import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BoardService {
 
     private final JdbcTemplateRepository jdbcTemplateRepository;
