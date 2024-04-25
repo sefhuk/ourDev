fb3d7eb6bbd4cd40df5b7e1b6e5ea728fef3a633
sefhuk <ish0411@naver.com>
2024-04-25 14:59:16 +0900

    feat: Post entity

--- src/main/java/hyuk/ourDev/OurDevApplication.java
+++ src/main/java/hyuk/ourDev/OurDevApplication.java
@@ -3,9 +3,11 @@ package hyuk.ourDev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableJpaAuditing
 public class OurDevApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/hyuk/ourDev/domain/post/entity/Post.java
@@ -0,0 +1,51 @@
+package hyuk.ourDev.domain.post.entity;
+
+import hyuk.ourDev.domain.BaseTimeEntity;
+import hyuk.ourDev.domain.board.entity.Board;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Builder
+    public Post(Long id, String title, String author, String content, Board board) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.content = content;
+        this.board = board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+        board.getPosts().add(this);
+    }
+}
