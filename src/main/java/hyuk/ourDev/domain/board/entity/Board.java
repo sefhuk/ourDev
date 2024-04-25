32a9ec4e3dd78feca2595c03c924c8c29cd82fe2
sefhuk <ish0411@naver.com>
2024-04-25 15:02:41 +0900

    feat: Board entity - Post entity 간 연관관계 추가

--- src/main/java/hyuk/ourDev/domain/board/entity/Board.java
+++ src/main/java/hyuk/ourDev/domain/board/entity/Board.java
@@ -1,11 +1,16 @@
 package hyuk.ourDev.domain.board.entity;
 
+import hyuk.ourDev.domain.post.entity.Post;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,6 +36,9 @@ public class Board {
 
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
 
     @Builder
     public Board(Long id, String name, String author, LocalDateTime createdAt,
