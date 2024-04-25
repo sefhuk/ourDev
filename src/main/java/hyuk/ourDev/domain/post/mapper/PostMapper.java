d697e1c0db30c6336f7092a27789a3c65af67a64
sefhuk <ish0411@naver.com>
2024-04-25 15:00:18 +0900

    feat: Post entity - dto 간 mapper

---
+++ src/main/java/hyuk/ourDev/domain/post/mapper/PostMapper.java
@@ -0,0 +1,14 @@
+package hyuk.ourDev.domain.post.mapper;
+
+import hyuk.ourDev.domain.post.dto.PostResponseDto;
+import hyuk.ourDev.domain.post.entity.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PostMapper {
+
+    @Mapping(source = "board.id", target = "boardId")
+    PostResponseDto PostToPostResponseDto(Post post);
+}
