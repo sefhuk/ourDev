418cf0df648b02830169fc008423a31de35a9f5a
sefhuk <ish0411@naver.com>
2024-05-01 18:15:06 +0900

    feat: 댓글 삭제 시 작성자 이름 확인하는 기능 구현

--- src/main/java/hyuk/ourDev/domain/comment/controller/CommentController.java
+++ src/main/java/hyuk/ourDev/domain/comment/controller/CommentController.java
@@ -8,11 +8,14 @@ import hyuk.ourDev.domain.post.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,8 +56,13 @@ public class CommentController {
 
     @DeleteMapping
     public ResponseEntity<Void> commentRemove(@RequestBody CommentRequest commentRequest) {
+        Comment comment = commentService.findComment(commentRequest.getCommentId());
 
-        commentService.removeComment(Long.parseLong(commentRequest.getCommentId()));
+        if (!comment.getAuthor().equals(commentRequest.getAuthor())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        commentService.removeComment(commentRequest.getCommentId());
         return ResponseEntity.ok().build();
     }
 }
