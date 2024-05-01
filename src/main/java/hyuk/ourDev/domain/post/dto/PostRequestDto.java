d7b9495a3068f24b101993e0d7184b2d4b9d6eb6
sefhuk <ish0411@naver.com>
2024-05-01 18:53:54 +0900

    feat: 게시글 entity에 암호 필드 추가, 게시글 추가 시 암호 입력하는 기능 구현

--- src/main/java/hyuk/ourDev/domain/post/dto/PostRequestDto.java
+++ src/main/java/hyuk/ourDev/domain/post/dto/PostRequestDto.java
@@ -9,11 +9,13 @@ public class PostRequestDto {
     private String title;
     private String author;
     private String content;
+    private Integer password;
 
     @Builder
-    public PostRequestDto(String title, String author, String content) {
+    public PostRequestDto(String title, String author, String content, Integer password) {
         this.title = title;
         this.author = author;
         this.content = content;
+        this.password = password;
     }
 }
